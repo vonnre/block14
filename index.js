e6fcc30a7ccae907f0c628909f8adc5b94732795
Brittany <111539361+vonnre@users.noreply.github.com>
2023-10-02 18:54:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+// // Only Odds: create an array with all input numbers. Create second array placeholder for odd numbers. Create loop to check modulus for not equal to 
+// // zero. Push results to odd numbers array.
+
+// const Numbers = [2, 4, 6, 8, 10, 11, 12];
+
+// const oddNums = [];
+
+// for (let i=0; i<Numbers.length; i++)
+//     {if(Numbers[i]%2!=0)
+//     {oddNums.push(Numbers[i])
+//     }
+// }
+// console.log(oddNums);
+
+
+// Vowel versus Consonant: create sentences that shows how many vowels and consonants are in a word. Create variables for the word, 
+// // the vowels, and the counts for consonants and vowels. Create loop to count each type and print the final resulting statement.
+
+// const word = "onomonopia";
+// const vowels = "aeiou";
+// let consonantCount =0;
+// let vowelCount =0;
+
+// for (let i=0; i<word.length; i++)
+// {if(vowels.indexOf(word[i]) !== -1)
+//     {vowelCount += 1;}
+//     else {consonantCount += 1;}
+// }
+// console.log(word + ' has '+ consonantCount + ' consonants and ' + vowelCount + ' vowels');
+
+//Reverse Array: With a for loop, create a JavaScript code that creates a new array in reverse order.
+//Set variables for original array and reverse array. Loop backwards thru the index to print the reverse array.
+
+// const origArr = [1, -1, 2, -3, 5, -8, 13];
+
+// const reverse = [];
+
+// for (let i=origArr.length-1; i>=0; i--)
+//     {reverse.push(origArr[i])
+//     }
+// console.log(reverse);
+
+
+//FizzBuzz: Create a JavaScript code that prints each number from 1 to 100 on a new line.
+// For each multiple of 3, print "Fizz" instead of the number.
+// For each multiple of 5, print "Buzz" instead of the number.
+// For numbers that are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+const num = 100;
+const fizzBuzz = [];
+ 
+for (i=0; i<= num; i++)
+{if(num)
+
+}
